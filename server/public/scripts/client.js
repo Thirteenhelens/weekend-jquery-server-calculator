4fd6f1614385f8fae51ae156858f4212fe69d48e
Jack Barrett <jbarrett1347@gmail.com>
2021-10-08 16:03:58 -0500

    Added server.js a bit late

---
+++ server/public/scripts/client.js
@@ -0,0 +1,10 @@
+$(document).ready(onLoad);
+
+function onLoad() {
+    //Is jQ sourced? 
+    console.log(`Hello!`);
+
+    // Click listeners
+    // $(`#clearButton`).on(`click`, );
+    // $(`#evalButton`).on(`click`, );
+}
